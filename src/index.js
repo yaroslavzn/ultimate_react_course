7a84c131c96df961be15a4034dfb829635b5caa7
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2023-10-23 11:12:30 +0300

    init how react works section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from "react-dom/client";
+// import ReactDOM from 'react-dom';
+
+import App from "./App";
+import "./index.css";
+
+// REACT 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// REACT 17
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
